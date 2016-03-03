7f8459be0ef420513f65c68c27cd95c08c8a1268
Julia Van Cleve <julia@jmv.me>
2016-03-03 12:25:51 -0800

    Update welcome message again

--- vagrant/scripts/welcome.sh
+++ vagrant/scripts/welcome.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "[35m[1mTo access your shared files use the following command: cd rubynd[m"
+echo "--------------------------------------------------------"
+echo "To access your shared files, type the command: cd rubynd"
+echo "--------------------------------------------------------"
